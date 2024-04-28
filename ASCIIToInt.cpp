03b3ed83109cc457c9048800ee712f7044637ce6
ShishiKola <119428628+ShishiKola@users.noreply.github.com>
2024-04-28 15:53:40 -0500

    Create ASCIIToInt.cpp

---
+++ ASCIIToInt.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <cctype> 
+#include <cstring> 
+#include <string>
+#include <cmath>
+using namespace std;
+int main(){
+   //========================== VARIABLES ===========================//  
+    string input; 
+    string num; 
+    int total;
+    int numlen;
+   //=========================================================//
+    cout << "input string: " << endl;
+    getline(cin, input);
+    for(int i = 0; i < input.length(); i++){
+        int c = input[i];
+        numlen = num.length(); 
+        cout << c; 
+        }
+        total = 0; 
+     return 0;
+    }
